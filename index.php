6988db2d512d965ca08b15cac2e5746cfe2811a3
Elogeek <elodiechristin@gmail.com>
2021-02-23 14:38:44 +0100

    EXO 189 les bases de données

--- index.php
+++ index.php
@@ -29,9 +29,6 @@
 
 // FIXME => Aucun code pour cette partie de l'exo, tout se passe sur PhpMyAdmin.
 
-
-
-
 /**
  * Créez via PhpMyAdmin une nouvelle base de données table_test_php
  * Créez dans cette base de données deux nouvelles tables   utilisateur  ET   produit VIA PHP, en passant par une requête.
@@ -60,8 +57,38 @@
  */
 
 // TODO Votre code ici.
+$dbname = "table_test_php";
+$server ="localhost";
+$password = " ";
+$user = "root";
 
 try {
-    ...
+
+    $conn = new PDO("mysql:host = $server;$dbname;charset=utf8",$user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "CREATE TABLE Utilisateur (
+        id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(20) NOT NULL,
+    prenom Varchar(20) NOT NULL, 
+    email VARCHAR(50),adresse VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    adresse VARCHAR(60) NOT NULL,
+    code postal VARCHAR(10) NOT NULL,
+    pays VARCHAR (50) NOT NULL,
+    date_join TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+
+    $sql = "CREATE TABLE produit (
+    id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(20) NOT NULL,
+    prix INT(20) DOUBLE NOT NULL,
+    description_courte VARCHAR(30) NOT NULL,
+    description_longue VARCHAR(60) NOT NULL 
+    )";
+
 }
-catch...
+
+catch(PDOException $exception) {
+    echo " Une erreur est survenue ! " .$exception->getMessage();
+}
